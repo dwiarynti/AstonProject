6039c55508434e7455823dee88b5005d1b74060d
Dwi Ariyanti <dwiariyanti121@gmail.com>
2017-09-05 17:18:53 +0700

    add pref

---
+++ Aston.Business/PrefComponent.cs
@@ -0,0 +1,27 @@
+﻿using Aston.Business.Data;
+using Aston.Entities;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Aston.Business
+{
+    public class PrefComponent
+    {
+        PrefExtensions _pref = new PrefExtensions();
+
+        public List<Pref> GetCategory()
+        {
+            return _pref.GetCategory();
+        }
+        public List<Pref> GetLocationType()
+        {
+            return _pref.GetLocationType();
+        }
+        public List<Pref> GetStatus()
+        {
+            return _pref.GetStatus();
+        }
+    }
+}
---
+++ Aston.Web/Process/PrefProcess.cs
@@ -0,0 +1,41 @@
+﻿using Aston.Entities;
+using Aston.Web.Base;
+using Microsoft.Extensions.Options;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net.Http;
+using System.Threading.Tasks;
+
+namespace Aston.Web.Process
+{
+    public class PrefProcess :ProcessComponent
+    {
+        private readonly AppSetting _serviceSettings;
+        public PrefProcess(IOptions<AppSetting> serviceSettings) : base(serviceSettings)
+        {
+            _serviceSettings = serviceSettings.Value;
+        }
+        public HttpResponseMessage GetCategory()
+        {
+            HttpResponseMessage result = default(HttpResponseMessage);
+            string requestUri = "api/pref/GetCategory/";
+            result = REST(requestUri, RESTConstants.GET);
+            return result;
+        }
+        public HttpResponseMessage GetLocationType()
+        {
+            HttpResponseMessage result = default(HttpResponseMessage);
+            string requestUri = "api/pref/GetLocationType/";
+            result = REST(requestUri, RESTConstants.GET);
+            return result;
+        }
+        public HttpResponseMessage GetStatus()
+        {
+            HttpResponseMessage result = default(HttpResponseMessage);
+            string requestUri = "api/pref/GetStatus/";
+            result = REST(requestUri, RESTConstants.GET);
+            return result;
+        }
+    }
+}
