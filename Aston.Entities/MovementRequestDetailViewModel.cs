78b637b67923cf9282827ba0d6f10fdaf5cdfaa4
Dwi Ariyanti <dwiariyanti121@gmail.com>
2017-09-08 16:46:23 +0700

    update movementrequest view model

--- Aston.Entities/MovementRequestDetailViewModel.cs
+++ Aston.Entities/MovementRequestDetailViewModel.cs
@@ -5,8 +5,20 @@ using System.Threading.Tasks;
 
 namespace Aston.Entities
 {
-    public class MovementRequestDetailViewModel : MovementRequestDetail
+    public class MovementRequestDetailViewModel
     {
+        public int ID { get; set; }
+        public int MovementRequestID { get; set; }
+        public string Description { get; set; }
+        public string AssetCategoryCD { get; set; }
+        public int Quantity { get; set; }
+        public int RequestTo { get; set; }
+        public string CreatedDate { get; set; }
+        public string CreatedBy { get; set; }
+        public string UpdatedDate { get; set; }
+        public string UpdatedBy { get; set; }
+        public string DeletedDate { get; set; }
+        public string DeletedBy { get; set; }
         public bool IsDelete { get; set; }
         public bool IsUpdate { get; set; }
     }
--- Aston.Entities/MovementRequestViewModel.cs
+++ Aston.Entities/MovementRequestViewModel.cs
@@ -5,9 +5,22 @@ using System.Threading.Tasks;
 
 namespace Aston.Entities
 {
-    public class MovementRequestViewModel : MovementRequest
-    {
-    
+    public class MovementRequestViewModel 
+    { 
+
+        public int ID { get; set; }
+        public string MovementDate { get; set; }
+        public string Description { get; set; }
+        public string ApprovedDate { get; set; }
+        public string ApprovedBy { get; set; }
+        public int StatusCD { get; set; }
+        public string Notes { get; set; }
+        public string CreatedDate { get; set; }
+        public string CreatedBy { get; set; }
+        public string UpdatedDate { get; set; }
+        public string UpdatedBy { get; set; }
+        public string DeletedDate { get; set; }
+        public string DeletedBy { get; set; }
         public List<MovementRequestDetailViewModel> MovementRequestDetail { get; set; }
     }
 }
